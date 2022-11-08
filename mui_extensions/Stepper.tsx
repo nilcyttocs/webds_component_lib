5fc4e4db7cd790cfc471a790c725cef0f3e3b718
Scott Lin <scott.lin@tw.synaptics.com>
2022-11-08 15:05:55 +0800

    Add custom stepper icon

--- mui_extensions/Stepper.tsx
+++ mui_extensions/Stepper.tsx
@@ -2,8 +2,44 @@ import React from "react";
 
 import Stepper, { StepperProps } from "@mui/material/Stepper";
 import StepConnector, { StepConnectorProps } from "@mui/material/StepConnector";
+import { StepIconProps } from "@mui/material/StepIcon";
+
+import { styled } from "@mui/material/styles";
 
 import { STEPPER_ATTRS } from "./constants";
+
+const VerticalStepIconRoot = styled("div")<{
+  ownerState: { completed?: boolean; active?: boolean };
+}>(({ theme, ownerState }) => ({
+  backgroundColor:
+    theme.palette.mode === "dark"
+      ? "rgba(255, 255, 255, 0.5)"
+      : "rgba(0, 0, 0, 0.38)",
+  width: STEPPER_ATTRS.ICON_SIZE,
+  height: STEPPER_ATTRS.ICON_SIZE,
+  zIndex: 1,
+  color: "#fff",
+  borderRadius: "50%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  ...((ownerState.active || ownerState.completed) && {
+    backgroundColor: "#007dc3"
+  })
+}));
+
+export const VerticalStepIcon = (props: StepIconProps) => {
+  const { active, completed, className } = props;
+
+  return (
+    <VerticalStepIconRoot
+      ownerState={{ completed, active }}
+      className={className}
+    >
+      {String(props.icon)}
+    </VerticalStepIconRoot>
+  );
+};
 
 interface VerticalStepConnectorProps extends StepConnectorProps {
   minConnectorHeight?: number | null;
